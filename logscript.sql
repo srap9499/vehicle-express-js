1b96dff5257c5471068ead14ee629d87f14c0e37
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-04-14 11:55:34 +0530

    created procedures for vehicle entry and exit

--- logscript.sql
+++ logscript.sql
@@ -6,3 +6,19 @@ CREATE TABLE IF NOT EXISTS vehiclelogs (
     PRIMARY KEY(id),
     CHECK (LENGTH(vehicle_id) > 6)
 );
+
+DELIMITER $$
+	CREATE PROCEDURE vehicle_entry(IN new_vehicle_id VARCHAR(12))
+    BEGIN
+		INSERT INTO vehiclelogs (vehicle_id, action)
+        VALUE (new_vehicle_id, 'ENTRY');
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+    CREATE PROCEDURE vehicle_exit(IN new_vehicle_id VARCHAR(12))
+    BEGIN
+		INSERT INTO vehiclelogs (vehicle_id, action)
+        VALUE (new_vehicle_id, 'EXIT');
+    END $$
+DELIMITER ;

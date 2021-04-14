f33adf3c15bc1d3c8790c536f8518d5ba03672c6
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-04-14 10:48:23 +0530

    Created app

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+
+// Express app
+const app = express();
+
+// Server Port
+const port = process.env.PORT | 3000;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port number: ${port}`);
+});
